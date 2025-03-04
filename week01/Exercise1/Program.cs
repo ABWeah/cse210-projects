ec5e4c3b9acce70135e5fbb6794a1d7d91c9afb2
Amos Bobby Weah <weahamosbobby@gmail.com>
2025-03-04 22:51:53 +0000

    Added input and output statements

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.Write("what is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.Write("what is your first name? ");
+        string first_name = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
