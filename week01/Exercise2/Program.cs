3abbf95848ca20cfee450c5bc91a1ed0ecccd024
Amos Bobby Weah <weahamosbobby@gmail.com>
2025-03-06 12:38:22 +0000

    Completion of Week one assignments.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,99 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percentage? ");
+        string userGradePoint = Console.ReadLine();
+        
+        int userGrade = int.Parse (userGradePoint);
+
+        string letter = "";
+        string sign = ""; 
+
+        if (userGrade >= 90)
+        {
+            letter = "an 'A";
+
+            if (userGrade <= 93)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+            // Console.WriteLine("PASSED. You scored an 'A'.");
+        }
+        else if (userGrade >= 80)
+        {
+            letter = "a 'B";
+             if (userGrade >= 87)
+            {
+                sign = "+";
+            }
+            else if (userGrade <= 83)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+            // Console.WriteLine("PASSED! You scored a 'B'. ");
+        }
+        else if (userGrade >= 70)
+        {
+            letter = "a 'C";
+             if (userGrade >= 77)
+            {
+                sign = "+";
+            }
+            else if (userGrade <= 73)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+            // Console.WriteLine("PASSED! You scored a 'C'.");
+        }
+        else if (userGrade >= 60)
+        {
+            letter = "a 'D";
+             if (userGrade >= 67)
+            {
+                sign = "+";
+            }
+            else if (userGrade <= 63)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+            // Console.WriteLine("Your grade is 'D'. You could better if you persist.");
+        }
+        else
+        {
+            letter = "an 'F";
+            //Console.WriteLine("Your grade is 'F'. You could do better if you persist.");
+        }
+
+        if (userGrade >= 70)
+        {
+            Console.WriteLine($"CONGRATULATIONS. You PASSED the course with {letter}{sign}' gradepoint.");
+        } 
+        else
+        {
+            Console.WriteLine($"SORRY! You could do better. You scored '{userGrade}' which is {letter}{sign}' gradepoint.");
+        }
+
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,67 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int>numbers = new List<int>();
+
+        int userNumber = -1;
+
+        while (userNumber != 0)
+        {
+            Console.Write("Add a number to the list. 0 when finished: ");
+
+            string response = Console.ReadLine();
+            userNumber = int.Parse(response);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        // Compute the sum of the number in the list
+
+        int sum = 0;
+        foreach(int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"Th sum is: {sum}");
+
+        //Compute the average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is {average}");
+
+        // Find the maximum number in the list.
+
+        int max = numbers[0];
+
+        foreach(int number in numbers)
+        {
+            if(number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The maximum number is {max}");
+
+        // Find the minimum number in the list
+        int min = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if(number < min)
+            {
+                min = number;
+            }
+        }
+        Console.WriteLine($"The minimum number is {min}");
+        
     }
 }
